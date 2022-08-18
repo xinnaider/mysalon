0630353a3901d5a1ea3397caf1b34eda59d02e73
Schnneider <86449199+xinnaider@users.noreply.github.com>
2022-08-17 23:53:56 -0300

    add

---
+++ Views/Cadastro.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using wpf_sallon.Views;
+
+namespace wpf_sallon.Views
+{
+    /// <summary>
+    /// Lógica interna para Cadastro.xaml
+    /// </summary>
+    public partial class Cadastro : Window
+    {
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        void OnClick1(object sender, RoutedEventArgs e)
+        {
+            MainWindow telas = new MainWindow();
+            this.Close();
+            telas.ShowDialog();
+        }
+    }
+}
