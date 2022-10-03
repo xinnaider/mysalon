c23d1ccd1c4264ed52ad3833716a8218ee27d4bd
LukasLS0 <l.sodre@estudante.ifro.edu.br>
2022-10-03 17:11:22 -0400

    login ta com erro

---
+++ Models/Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_sallonnovo.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public Cliente Cliente { get; set; }
+        private static Login _instance;
+
+        private Login() { }
+
+        public static Login GetInstance()
+        {
+            if (_instance == null)
+                _instance = new Login();
+
+            return _instance;
+        }
+
+        public static bool Loginn(string usuario, string senha)
+        {
+            var user = new LoginDAO().GetByUsuario(usuario, senha);
+
+            if (user != null)
+                return true;
+
+            return false;
+        }
+
+        public static int GetFuncionarioId()
+        {
+            return _instance.Cliente.Id;
+        }
+    }
+}
