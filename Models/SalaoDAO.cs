e6c7f5d409b4f04334fd9073187c1176e77bf691
Schnneider <fernandoschnneider@gmail.com>
2022-10-03 10:16:36 -0400

    Update SalaoDAO.cs

--- Models/SalaoDAO.cs
+++ Models/SalaoDAO.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using wpf_sallonnovo.Helpers;
+using wpf_sallonnovo.Models;
+using wpf_sallonnovo.bancodados;
+using MySql.Data.MySqlClient;
 
 namespace wpf_sallonnovo.Models
 {
-    public class SalaoDAO
+    class SalaoDAO
     {
+        private static conexao _conn = new conexao();
+
+        public List<Salao> List()
+        {
+            try
+            {
+                var lista = new List<Salao>();
+                var comando = _conn.Query();
+
+                comando.CommandText = "SELECT * FROM Salao";
+
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var salao = new Salao();
+
+                    salao.Id = reader.GetInt32("id_sal");
+                    salao.Nome = DAOHelper.GetString(reader, "nome_sal");
+                    salao.Telefone = DAOHelper.GetString(reader, "telefone_sal");
+                    salao.Proprietario = DAOHelper.GetString(reader, "proprietario_sal");
+                    salao.Razao_Social = DAOHelper.GetString(reader, "razao_social_sal");
+                    salao.CNPJ = DAOHelper.GetString(reader, "cnpj_sal");
+                    salao.Email = DAOHelper.GetString(reader, "email_sal");
+                    salao.Descricao = DAOHelper.GetString(reader, "descriçao_sal");
+
+                    lista.Add(salao);
+                }
+
+                reader.Close();
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
