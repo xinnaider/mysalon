b70bba746e00aede4f75c60f9e84cccbdc1df19c
AntonioPalauroSampaio <antonio.sampaio155@gmail.com>
2022-10-04 09:17:32 -0400

    Falaram que nao era pra mecher na terceira table entao ficou assim

--- Models/AgendaServicoDAO.cs
+++ Models/AgendaServicoDAO.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using wpf_sallonnovo.bancodados;
 
 namespace wpf_sallonnovo.Models
 {
     internal class AgendaServicoDAO
     {
+        private static conexao _conn = new conexao();
 
+        public void Insert(AgendaServicoDAO agendaServico)
+        {
+            try
+            {
+                var comando = _conn.Query();
+
+                comando.CommandText = "insert into agenda values (null, @quantidade, @servico, @agenda);";
+
+                comando.Parameters.AddWithValue("@quantidade", agendaServico);
+                
+
+                var resultado = comando.ExecuteNonQuery();
+
+                if (resultado == 0)
+                {
+                    throw new Exception("Deu erro no momento de salvar as informações");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
     }
 }
