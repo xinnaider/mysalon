8ed9acf7e7be359df7b2e3f09855e86d1a9b43d4
Luana Freire <102596561+LuanaBscarmoon@users.noreply.github.com>
2022-10-11 13:45:48 -0400

    Insert de Dados do Cliente

--- Views/Registro.xaml.cs
+++ Views/Registro.xaml.cs
@@ -22,10 +22,18 @@ namespace wpf_sallonnovo.Views
     public partial class Registro : Window
     {
         private Cliente _cliente = new Cliente();
-      // private Login _login = new Login();
+     
         public Registro()
         {
             InitializeComponent();
+            Loaded += Registro_Loaded;
+        }
+
+        public Registro(Cliente cliente)
+        {
+            InitializeComponent();
+            _cliente = cliente;
+            Loaded += Registro_Loaded;
         }
 
         void OnClick1(object sender, RoutedEventArgs e)
@@ -35,11 +43,73 @@ namespace wpf_sallonnovo.Views
             telas.ShowDialog();
         }
 
+        private void Registro_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            txtNome.Text = _cliente.Nome;
+            txtCPF.Text = _cliente.CPF;
+            txtRG.Text = _cliente.RG;
+            txtTelefone.Text = _cliente.Telefone;
+            txtEmail.Text = _cliente.Email;
+
+            if (_cliente.Sexo == "Masculino")
+            {
+                rbMasculino.IsChecked = true;
+            }
+            else
+            {
+                if (_cliente.Sexo == "Femenino")
+                {
+                    rbFeminino.IsChecked = true;
+                }
+                else rbNDizer.IsChecked = true;
+            }
+
+        }
+
         private void btnRegistrar_Click(object sender, RoutedEventArgs e)
         {
-          _cliente.Nome = txtNome.Text;
-          _cliente.CPF = txtCPF.Text;
-            
+          
+           
+
+            _cliente.Nome = txtNome.Text;
+            _cliente.CPF = txtCPF.Text;
+            _cliente.RG = txtRG.Text;
+            _cliente.Telefone = txtTelefone.Text;
+            _cliente.Email = txtEmail.Text;
+
+            if ((bool)rbMasculino.IsChecked)
+                _cliente.Sexo = "Masculino";
+            else
+            {
+                if ((bool)rbFeminino.IsChecked)
+                    _cliente.Sexo = "Femenino";
+                else _cliente.Sexo = "Prefiro não dizer";
+            }
+
+            try
+            {
+                var dao = new ClienteDAO();
+
+                if (_cliente.Id > 0)
+                {
+                    dao.Update(_cliente);
+                    MessageBox.Show("Registro de cliente atualizado com sucesso!");
+                }
+                else
+                {
+                    dao.Insert(_cliente);
+                    MessageBox.Show("Registro de cliente cadastrado com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
         }
     }
 }
