041d3838ece92901dcc277c9c24ab54d2c5f7235
LukasLS0 <l.sodre@estudante.ifro.edu.br>
2022-11-16 16:52:27 -0400

    daoRegistro
    funcionando

---
+++ Models/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_sallonnovo.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+
+        public int FkCliente { get; set; }
+    }
+}
